03d259aa1dda4e0784cfe3a768fba61877be503e
Nicholas <nick.fernandez@wsu.edu>
2020-08-06 18:37:19 -0700

    Adding practice project

---
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You Clicked {count} times.";
+        }
+
+    }
+}
