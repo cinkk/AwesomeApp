984a6f3e1496213516ef47f7372f87f3f2591e76
Nicholas Fernandez <nicholas.rueben@gmail.com>
2020-08-27 20:10:06 -0700

    renamed all to cardlector, wrte notes about collection and set interaction

---
+++ Cardlector/Cardlector/NoteEntryPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Cardlector.Models;
+
+namespace Cardlector
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        //This will we used to bind a set to a collection
+        //public int collectionID;
+        public NoteEntryPage(/**pass in colletionID here**/)
+        {
+            InitializeComponent();
+            //set collectionID
+            //this.collectionID = collectionID;
+        }
+
+        /**
+         * This will get the sets and return all that match the collecionID
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetSetsAsync(this.collectionID);
+        }
+         **/
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Cardlector/Cardlector/NotesPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cardlector.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cardlector
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+
+        //Need to pass a collection identifier to the created SET page
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
